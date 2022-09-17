53ad8f55b8481f7e33ef344ac02ed8e12b2fba48
Derecky Costa <dereckycosta@gmail.com>
2022-09-17 05:29:40 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle="dark-content" />
+      <Text>Teste 123</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
